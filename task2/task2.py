a13567b82e3977ff3b8bbc2d4f37455b7a32695e
Max Reshetnyk <86986643+mreshetnyk@users.noreply.github.com>
2021-07-11 20:56:16 +0300

    Create task2.py

---
+++ task2/task2.py
@@ -0,0 +1,22 @@
+print("Калькулятор")
+print("Щоб завершити програму введіть 0")
+while True:
+    s = input("Знак (+,-,*,/): ")
+    if s == '0':
+        break
+    if s in ('+', '-', '*', '/'):
+        x = float(input("x="))
+        y = float(input("y="))
+        if s == '+':
+            print("%.2f" % (x+y))
+        elif s == '-':
+            print("%.2f" % (x-y))
+        elif s == '*':
+            print("%.2f" % (x*y))
+        elif s == '/':
+            if y != 0:
+                print("%.2f" % (x/y))
+            else:
+                print("Error")
+    else:
+        print("Неправильно введене значення")
